ca14ba46e1ddcaeced7bca79f8d1630905a818de
kelvin.M.camargo <kelvin.m.camargo@gmail.com>
2025-04-24 23:23:56 -0300

    feat: testing the api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import React, { useState, useEffect } from 'react';
 
+function GitHubUser() {
+  const [userData, setUserData] = useState(null); 
+  const username = 'kelvin-camargo'; 
 
-function App() {
+  useEffect(() => {
+
+    fetch(`https://api.github.com/users/${username}`)
+      .then(response => response.json()) 
+      .then(data => setUserData(data)) 
+      .catch(error => console.error('Erro ao buscar dados do GitHub:', error));
+  }, []);
+
+  if (!userData) {
+    return <p>Carregando...</p>;
+  }
+
   return (
-    <>
-      <p></p>  
-    </>
-  )
+    <div style={{ textAlign: 'center' }}>
+      <p>@{userData.login}</p>
+      <img src={userData.avatar_url} alt="Avatar" style={{ width: '150px', borderRadius: '50%' }} />
+
+      <p>Bio: {userData.bio || "Esse usuario não possue uma bio"}</p>
+    </div>
+  );
 }
 
-export default App
+export default GitHubUser;
